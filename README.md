89489f842a3f2bad849644331e77bc6e5d28e34f
Jim Plush <jiminoc@gmail.com>
2015-02-23 15:27:56 -0800

    iniitial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+ratelimiter
+=====
+
+Package ratelimiter implements an LRU based ratelimiter for near-cache type workloads. It's aim is to marry LRU based local caching with Redis's INCR command for incrmenting counters. There are times during data processing where you'd like to be able to say things like "If I see this thing n times in an hour, I don't care about it anymore", or the classic API rate limiting example. Once you hit a certain level of throughput you'd also like to reduce your network hops over to a remote service. 
+This package aims to support both kids of workloads. To get the best and most accurate results it's assumed that your keys are partitioned in a way where the same calls go to the same box. The data used in ratelimiter is local to the machine it's on. It's also appromixation rate limiting vs keeping track of any time windows or exact time boundaries. 
+
+The logic is simply.. we'll keep incrementing you, once you pass the max count we're allowing we'll check the time of your key update and compare against the duration difference between now and the rate period you passed in. If it's been longer than that time period we'll reset your count and update the time to give you a fresh set of credits. 
+
+The underlying structure is based on groupcache's LRU library. 
+
+Installation
+------------
+
+    go get github.com/CrowdStrike/ratelimiter
+
+
+Features
+--------
+
+* Ability to set a rate limiting time period
+* Ability to disable time periods and say once you're ratelimited, you're done
+* You can set a maxsize so your memory footprint can remain constant, most used keys stay hot in cache
+
+
+Example of correct usage where you want to only allow 100 requests per hour for a given key. Note: If you want to disable lifting the rate period set ratePeriod := 0
+That will effectively say for the lifetime of the process if you hit the rate limit you're done. 
+```go
+    maxCapacity := 1000
+	ratePeriod := 1 * time.Hour
+	rl, err := New(maxCapacity, ratePeriod)
+	if err != nil {
+		fmt.Printf("Unable to create cache")
+	}
+
+	userKey := "user123"
+	maxCount = 100 // the maximum number of items I want from this user in one hour
+	cnt, underRateLimit := rl.Incr(userKey, maxCount)
+	if underRateLimit {
+		// allow further access
+		...
+	} else {
+		fmt.Printf("User [%s] is over rate limit, denying for now, current count [%d]", userKey, cnt)
+	}
+```
+
---
+++ lrucounter_test.go
@@ -0,0 +1,229 @@
+package ratelimiter
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestEmptyCacheErrors(t *testing.T) {
+	_, err := New(0, 100*time.Second)
+	if err == nil {
+		t.Fatalf("expected a maxentry size of 0 would fail Cache creation")
+	}
+}
+
+func TestIncr(t *testing.T) {
+	rl, err := New(100, 2*time.Second)
+	if err != nil {
+		t.Fatalf("Cache should have been created OK")
+	}
+
+	key := "foo"
+	maxCount := 100
+	cnt, ok := rl.Incr(key, maxCount)
+	if cnt != 1 {
+		t.Fatalf("count should have been [1] actual [%d]", cnt)
+	}
+	if !ok {
+		t.Fatalf("expected a brand new key would not be rate limited")
+	}
+
+	cnt, ok = rl.Incr(key, maxCount)
+	if cnt != 2 {
+		t.Fatalf("count should have been [2] actual [%d]", cnt)
+	}
+
+}
+
+// expected that incrementing a key 100 times within 10 seconds
+// returns a false for the ratelimiting status
+func TestBasicRateLimiting(t *testing.T) {
+
+	rl, _ := New(100, 10*time.Second)
+
+	maxCount := 10
+	key := "foo"
+	for i := 0; i < 15; i++ {
+		cnt, underRateLimit := rl.Incr(key, maxCount)
+		if int(cnt) > maxCount && underRateLimit {
+			t.Fatalf("expected that if we went over [%d] increments ratelimit would be false, but was true", maxCount)
+		}
+
+	}
+}
+
+func TestMaxItemsInCache(t *testing.T) {
+	maxItemsInCache := 10
+	rl, _ := New(maxItemsInCache, 10*time.Second)
+
+	for i := 0; i < 15; i++ {
+		key := fmt.Sprintf("foo_%d", i)
+		_, _ = rl.Incr(key, 1)
+	}
+
+	if rl.Len() > maxItemsInCache {
+		t.Fatalf("expected to only have [%d] items in cache, actually got [%d]", maxItemsInCache, rl.Len())
+	}
+
+}
+
+func TestGet(t *testing.T) {
+	maxItemsInCache := 10
+	rl, _ := New(maxItemsInCache, 10*time.Second)
+
+	key := "foo"
+	_, _ = rl.Incr(key, 10)
+
+	cnt, _ := rl.Get(key)
+	if cnt != 1 {
+		t.Fatalf("expected to get foo with a count of [1] but got [%d]", cnt)
+	}
+
+	_, _ = rl.Incr(key, 10)
+
+	cnt, _ = rl.Get(key)
+	if cnt != 2 {
+		t.Fatalf("expected to get foo with a count of [2] but got [%d]", cnt)
+	}
+
+}
+
+func TestRemove(t *testing.T) {
+	maxItemsInCache := 10
+	rl, _ := New(maxItemsInCache, 10*time.Second)
+
+	key := "foo"
+	_, _ = rl.Incr(key, 10)
+
+	cnt, _ := rl.Get(key)
+	if cnt != 1 {
+		t.Fatalf("expected to get foo with a count of [1] but got [%d]", cnt)
+	}
+
+	rl.Remove(key)
+
+	_, ok := rl.Get(key)
+	if ok {
+		t.Fatalf("should have gotten false back since I deleted the key")
+	}
+
+}
+
+// by setting a 0 second duration we're saying we don't want to ever clear the rate limit
+// once you're rate limited you're done
+func TestRateLimitDoesntRemove(t *testing.T) {
+
+	rl, _ := New(100, 0)
+
+	maxCount := 10
+	key := "foo"
+
+	_, underRateLimit := rl.Incr(key, maxCount)
+
+	if !underRateLimit {
+		t.Fatalf("expected for a new key that incrementing the first time would keep us under the ratelimit")
+	}
+
+	// push us over the rate limit
+	for i := 0; i < 15; i++ {
+		_, _ = rl.Incr(key, maxCount)
+	}
+
+	_, underRateLimit = rl.Incr(key, maxCount)
+
+	if underRateLimit {
+		t.Fatalf("expected that if we went over [%d] increments ratelimit would be true, but was false", maxCount)
+	}
+
+	// sleep for 3 seconds and we should be OK again
+	time.Sleep(3 * time.Second)
+	cnt, underRateLimit := rl.Incr(key, maxCount)
+
+	if underRateLimit {
+		t.Fatalf("expected that since we don't went to apply a rate limit we'd still be rate limited [%d]", cnt)
+	}
+
+}
+
+// ensure that after n seconds our rate limit no longer applies
+func TestRateLimitGetsRemoved(t *testing.T) {
+
+	rl, _ := New(100, 2*time.Second)
+
+	maxCount := 10
+	key := "foo"
+
+	_, underRateLimit := rl.Incr(key, maxCount)
+
+	if !underRateLimit {
+		t.Fatalf("expected for a new key that incrementing the first time would keep us under the ratelimit")
+	}
+
+	// push us over the rate limit
+	for i := 0; i < 15; i++ {
+		_, _ = rl.Incr(key, maxCount)
+	}
+
+	_, underRateLimit = rl.Incr(key, maxCount)
+
+	if underRateLimit {
+		t.Fatalf("expected that if we went over [%d] increments ratelimit would be true, but was false", maxCount)
+	}
+
+	// sleep for 3 seconds and we should be OK again
+	time.Sleep(3 * time.Second)
+	cnt, underRateLimit := rl.Incr(key, maxCount)
+
+	if !underRateLimit {
+		t.Fatalf("expected that if we slept for a while to pass the ttl that we'd be ok again but our count was [%d]", cnt)
+	}
+
+	for i := 0; i < 15; i++ {
+		_, _ = rl.Incr(key, maxCount)
+	}
+
+	_, underRateLimit = rl.Incr(key, maxCount)
+
+	if underRateLimit {
+		t.Fatalf("expected that if we went over [%d] increments ratelimit would be true, but was false", maxCount)
+	}
+
+	// sleep for 3 seconds and we should be OK again
+	time.Sleep(3 * time.Second)
+	cnt, underRateLimit = rl.Incr(key, maxCount)
+	if !underRateLimit {
+		t.Fatalf("expected that if we slept for a while to pass the ttl that we'd be ok again but our count was [%d]", cnt)
+	}
+
+}
+
+// BENCHMARKS
+// go test -bench=. -run=XXX
+// on macbook pro ~2.7 million ops a second
+func BenchmarkIncrWithPeriod(b *testing.B) {
+	rl, _ := New(100, 2*time.Second)
+	maxCount := 10
+	key := "foo"
+	for n := 0; n < b.N; n++ {
+		rl.Incr(key, maxCount)
+	}
+}
+
+// on macbook pro ~3 million ops a second
+func BenchmarkIncrWithoutPeriod(b *testing.B) {
+	rl, _ := New(100, 0)
+	maxCount := 10
+	key := "foo"
+	for n := 0; n < b.N; n++ {
+		rl.Incr(key, maxCount)
+	}
+}
+
+func BenchmarkGet(b *testing.B) {
+	rl, _ := New(100, 2*time.Second)
+	key := "foo"
+	for n := 0; n < b.N; n++ {
+		_, _ = rl.Get(key)
+	}
+}
